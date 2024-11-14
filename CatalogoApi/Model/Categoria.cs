42991876e5491a21b0c28c7c3574cedab001a6c3
Samuel Juren <sammjureen@outlook.com>
2024-11-14 17:38:59 -0300

    Relacionamento das tabelas

--- CatalogoApi/Model/Categoria.cs
+++ CatalogoApi/Model/Categoria.cs
@@ -1,8 +1,17 @@
+using System.Collections.ObjectModel;
+
 namespace CatalogoApi.Model;
 
 public class Categoria
 {
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+    
     public int CategoriaId { get; set; }
     public string? Nome { get; set; }
     public string? ImageUrl { get; set; }
+    
+    public ICollection<Produto>? Produtos { get; set; }
 }
