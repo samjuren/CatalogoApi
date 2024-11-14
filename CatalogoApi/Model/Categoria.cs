32fc7cb462c234a787f7e24b90884e532b4eb74d
Samuel Juren <sammjureen@outlook.com>
2024-11-13 21:25:36 -0300

    Model e NuGets

---
+++ CatalogoApi/Model/Categoria.cs
@@ -0,0 +1,8 @@
+namespace CatalogoApi.Model;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImageUrl { get; set; }
+}
