8c2ad89671e2d79cf4c86f4a6292cd4c3e68548c
Samuel Juren <sammjureen@outlook.com>
2024-11-14 23:47:12 -0300

    Criado migrations e colocado codigo sql na migrations

---
+++ CatalogoApi/Migrations/20241115023538_PopulaCategorias.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogoApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("insert into Categorias (Nome, ImageUrl) Values ('Bebidas', 'bebidas.jpg')");
+            mb.Sql("insert into Categorias (Nome, ImageUrl) Values ('Laches', 'lanches.jpg')");
+            mb.Sql("insert into Categorias (Nome, ImageUrl) Values ('Sobremesas', 'sobremesas.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("delete from Categorias");
+        }
+    }
+}
