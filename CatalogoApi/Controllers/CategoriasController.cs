9c19babb5495e799455c2dbec42b762639a7fde8
Samuel Juren <sammjureen@outlook.com>
2024-11-17 16:42:16 -0300

    Nova controller (Categorias) e implementados os mesmos endpoints do Produto.
    Ajuste do Json

---
+++ CatalogoApi/Controllers/CategoriasController.cs
@@ -0,0 +1,91 @@
+using CatalogoApi.Context;
+using CatalogoApi.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogoApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("Produtos")]
+        public ActionResult<IEnumerable<Categoria>> GetCategoriasEProdutos()
+        {
+            return _context.Categorias
+                .Include(p => p.Produtos)
+                .ToList();
+        }
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> GetCategorias()
+        {
+            var categorias = _context.Categorias.ToList();
+
+            if (categorias is null)
+                return NotFound("Produtos não encontrados");
+            
+            return categorias;
+        }
+        
+        [HttpGet("{id:int}", Name = "ObterCategoria")]
+        public ActionResult<Categoria> GetCategoriaById(int id)
+        {
+            var categorias = _context.Categorias.FirstOrDefault(x => x.CategoriaId == id);
+
+            if (categorias is null)
+            {
+                return NotFound("Produto não encontrado");
+            }
+            
+            return categorias;
+        }
+        
+        [HttpPost]
+        public ActionResult Post(Categoria categorias)
+        {
+            if (categorias is null)
+                return BadRequest();
+            
+            _context.Categorias.Add(categorias);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto", 
+                new { id = categorias.CategoriaId }, categorias);
+        }
+        
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Categoria categorias)
+        {
+            if (id != categorias.CategoriaId)
+                return BadRequest();
+            
+            _context.Entry(categorias).State = EntityState.Modified;
+            _context.SaveChanges();
+            
+            return Ok(categorias);
+        }
+        
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(x => x.CategoriaId == id);
+            
+            if (categoria is null)
+                return NotFound("Produto não encontrado");
+            
+            _context.Categorias.Remove(categoria);
+            _context.SaveChanges();
+            
+            return Ok(categoria);
+        }
+    }
+}
--- CatalogoApi/Model/Produto.cs
+++ CatalogoApi/Model/Produto.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
 
 namespace CatalogoApi.Model;
 
@@ -31,5 +32,6 @@ public class Produto
     public int? CategoriaId { get; set; }
     
     //PK
+    [JsonIgnore]
     public Categoria? Categoria { get; set; }
 }
